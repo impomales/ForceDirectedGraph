f92fd1f23d3ddeccce89027b04e142a1d95fa55c
impomales <impomales@gmail.com>
2016-07-26 18:58:40 -0400

    flags displayed and draggable

--- js/index.js
+++ js/index.js
@@ -2,9 +2,19 @@ var url = "https://raw.githubusercontent.com/DealPete/forceDirected/master/count
 
 var width = 1000, height = 800;
 
+var container = d3.select('#container');
+
 var svg = d3.select('svg')
   .attr('width', width)
   .attr('height', height);
+
+var tip = d3.tip()
+  .attr('class', 'tip')
+  .html(function(d) {
+    return "<p>" + d.country + "</p>";
+  });
+
+svg.call(tip);
 
 d3.json(url, function(error, graph) {
   if (error) throw error;
@@ -21,17 +31,17 @@ d3.json(url, function(error, graph) {
     .enter().append('line')
     .attr('class', 'link');
   
-  var node = svg.selectAll('.node')
+  var node = container.select('.box').selectAll('.node')
     .data(graph.nodes)
-    .enter().append('rect')
-    .attr('class', 'node')
+    .enter().append('img')
+    .attr('class', function(d) {return 'flag flag-' + d.code})
+    .on('mouseover', tip.show)
+    .on('mouseout', tip.hide)
+    .call(force.drag);
   
-  force.on('end', function() {
-    node.attr('x', function(d) {return d.x;})
-    node.attr('y', function(d) {return d.y;})
-    node.attr('width', 16)
-    node.attr('height', 11)
-    node.text('hello');
+  force.on('tick', function() {
+    node.style('left', function(d) {return (d.x - 10) + 'px';})
+      .style('top', function(d) {return (d.y - 810) + 'px';});
     
     link.attr('x1', function(d) {return d.source.x;})
       .attr('y1', function(d) {return d.source.y;})
